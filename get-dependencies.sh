84c990ae08c9e25e8f21f5efbe984d75c13e783c
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-02-03 23:51:36 -0400

    refactor to template (#24)

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,26 +1,21 @@
 #!/bin/sh
 
-set -eux
-ARCH="$(uname -m)"
-EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+set -eu
+ARCH=$(uname -m)
 
-echo "Installing build dependencies..."
+echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-	base-devel           \
 	boost                \
 	boost-libs           \
 	catch2               \
 	ccache               \
 	clang                \
 	cmake                \
-	curl                 \
 	crypto++             \
 	doxygen              \
-	ffmpeg               \
 	fmt                  \
 	gamemode             \
-	git                  \
 	glslang              \
 	glu                  \
 	graphviz             \
@@ -28,19 +23,17 @@ pacman -Syu --noconfirm \
 	libinih              \
 	libvpx               \
 	libxi                \
-	libxkbcommon-x11     \
 	libxss               \
 	libzip               \
 	lld                  \
 	llvm                 \
-	mesa                 \
 	meson                \
 	ninja                \
 	nlohmann-json        \
 	pipewire-audio       \
+	pipewire-jack        \
 	pulseaudio           \
 	pulseaudio-alsa      \
-	qt6-base             \
 	qt6ct                \
 	qt6-multimedia       \
 	qt6-tools            \
@@ -55,13 +48,67 @@ pacman -Syu --noconfirm \
 	xcb-util-image       \
 	xcb-util-renderutil  \
 	xcb-util-wm          \
-	xorg-server-xvfb     \
-	zip                  \
-	zsync
+	zip
 
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
-chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-common
+get-debloated-pkgs --add-common ffmpeg-mini
+
+echo "Building Azahar..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/azahar-emu/azahar.git"
+
+case "$ARCH" in
+	x86_64)  set -- -march=x86-64-v3 -O3 -flto=thin -fuse-ld=lld -DNDEBUG;;
+	aarch64) set -- -march=armv8-a -mtune=generic -O3 -flto=thin -fuse-ld=lld -DNDEBUG;;
+	*)       >&2 echo "ERROR: Unknown arch: $ARCH"; exit 1;;
+esac
+
+git clone --recursive "$REPO" ./azahar
+cd ./azahar
+
+if [ "${DEVEL_RELEASE-}" = 1 ]; then
+	git rev-parse --short HEAD > ~/version
+else
+	git fetch --tags origin
+	TAG=$(git tag --sort=-v:refname | grep -vi 'rc\|alpha' | head -1)
+	git checkout "$TAG"
+	echo "$TAG" > ~/version
+fi
+
+git submodule update --init --recursive -j$(nproc)
+
+# HACK
+sed -i '10a #include <memory>' ./src/video_core/shader/shader_jit_a64_compiler.*
+# HACK2
+sed -i '1i find_package(SPIRV-Headers)' ./externals/sirit/sirit/src/CMakeLists.txt
+# HACK3
+qpaheader=$(find /usr/include -type f -name 'qplatformnativeinterface.h' -print -quit)
+sed -i "s|#include <qpa/qplatformnativeinterface.h>|#include <$qpaheader>|" ./src/citra_qt/bootmanager.cpp
+
+mkdir -p ./build
+cd ./build
+
+cmake ../ \
+	-G Ninja                             \
+	-DCMAKE_CXX_COMPILER=clang++         \
+	-DCMAKE_C_COMPILER=clang             \
+	-DCMAKE_INSTALL_PREFIX=/usr          \
+	-DENABLE_QT_TRANSLATION=ON           \
+	-DUSE_SYSTEM_BOOST=OFF               \
+	-DCMAKE_BUILD_TYPE=Release           \
+	-DUSE_SYSTEM_VULKAN_HEADERS=ON       \
+	-DENABLE_LTO=OFF                     \
+	-DENABLE_TESTS=OFF                   \
+	-DENABLE_ROOM_STANDALONE=OFF         \
+	-DUSE_SYSTEM_GLSLANG=ON              \
+	-DCITRA_USE_PRECOMPILED_HEADERS=OFF  \
+	-DCMAKE_C_COMPILER_LAUNCHER=ccache   \
+	-DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
+	-DCMAKE_C_FLAGS="$*"                 \
+	-DCMAKE_CXX_FLAGS="$*"               \
+	-Wno-dev
+
+ninja
+sudo ninja install
---
+++ make-appimage.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -eu
+
+ARCH=$(uname -m)
+export ARCH
+export ADD_HOOKS="self-updater.bg.hook:x86-64-v3-check.hook"
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export APPNAME=Azahar
+export DESKTOP=/usr/share/applications/org.azahar_emu.Azahar.desktop
+export ICON=/usr/share/icons/hicolor/512x512/apps/org.azahar_emu.Azahar.png
+export DEPLOY_OPENGL=1
+export DEPLOY_VULKAN=1
+export DEPLOY_PIPEWIRE=1
+
+# Deploy dependencies
+quick-sharun /usr/bin/azahar* /usr/lib/libgamemode.so*
+
+# Additional changes can be done in between here
+
+# Turn AppDir into AppImage
+quick-sharun --make-appimage
